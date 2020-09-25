d819cc41338722a2a9871f455048b5d8a4478ef4
S F <sebastian.fariasb@usach.cl>
2020-09-25 16:21:20 -0300

    finished challenge

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const search = document.querySelector('#search')
+const input = document.querySelector('.search-bar')
 const dataValues = document.querySelectorAll('h3')
 const IP = 0
 const LOCATION = 1
@@ -6,60 +7,74 @@ const TIMEZONE = 2
 const ISP = 3
 const ipRegExp = new RegExp('^([0-9]{1,3}\.){3}[0-9]{1,3}$','i')
 const domainRegExp = new RegExp('(?:(?:(?<thld>[\w\-]*)(?:\.))?(?<sld>[\w\-]*))\.(?<tld>[\w\-]*)','i')
+const API_URL = 'https://geo.ipify.org/api/v1'
+const MAP_TOKEN = 'pk.eyJ1IjoieGVvbnJpZGVyIiwiYSI6ImNrZmkydTBnNzAzd24yd3FiNXRzb21laGgifQ.owsPKYbZ0PT_6fgOqYQ_fg'
+const IP_API_KEY ='at_qCntz3vCXjuld1TlzkCPJwBwU2Z84'
 
+window.addEventListener("DOMContentLoaded", () => init());
+search.addEventListener('submit', event => handleSubmit(event))
 
-window.addEventListener("DOMContentLoaded", () => {init()});
-document.getElementById('search').addEventListener('submit', event => {handleSubmit(event)})
-
-const moveMap = (lat,lon) =>{
-    const mymap = L.map('mapid').setView([lat, lon], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoieGVvbnJpZGVyIiwiYSI6ImNrZmkydTBnNzAzd24yd3FiNXRzb21laGgifQ.owsPKYbZ0PT_6fgOqYQ_fg', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
-let myIcon = L.icon({
-    iconUrl: 'images/icon-location.svg',
-    iconSize: [46,55]
-})
-let marker = L.marker([lat, lon],{icon:myIcon}).addTo(mymap);
-}
 const init = () => {
-    fetch('https://ipapi.co/json/').then( res => {
-        return res.json();
-    }).then( data => {
-        const { ip, city, region_code, latitude, longitude, utc_offset, org} = data
-        const formattedUTC = `${utc_offset.substring(0,3)}:${utc_offset.substring(3,5)}`
-        dataValues[IP].innerHTML = ip
-        dataValues[LOCATION].innerHTML = `${city}, ${region_code}`
-        dataValues[TIMEZONE].innerHTML = `UTC ${formattedUTC}`
-        dataValues[ISP].innerHTML = org
-        moveMap(latitude,longitude)
-    })
-}
-const findIP = ipToFind => {
-    if(ipRegExp.test(ipToFind)){
-console.log(`ip: ${ipToFind}`);
-}
-else{
-    console.log(`domain: ${ipToFind}`);
-    }
-    // dataValues[IP].innerHTML = ip
-    // dataValues[LOCATION].innerHTML = `${city}, ${region_code}`
-    // dataValues[TIMEZONE].innerHTML = `UTC ${formattedUTC}`
-    // dataValues[ISP].innerHTML = org
-    // moveMap(latitude,longitude)
+    fetch(`${API_URL}?apiKey=${IP_API_KEY}`).then( res => {
+            return res.json()
+        }).then( data => {
+            consumeAPI(data)
+        })
 }
 const handleSubmit = e => {
     e.preventDefault()
-    const ipToFind= e.target.querySelector('input').value
-    e.target.querySelector('input').value = ''
+    const ipToFind= input.value
+    input.value = ''
     if(ipRegExp.test(ipToFind) || domainRegExp.test(ipToFind)){
         findIP(ipToFind)
     }else{
         alert('Please, enter a valid IP Address or domain')
     }
+}
+const findIP = ipToFind => {
+    if(ipRegExp.test(ipToFind)){
+        fetch(`${API_URL}?apiKey=${IP_API_KEY}&ipAddress=${ipToFind}`).then( res => {
+            return res.json()
+        }).then( data => {
+            consumeAPI(data)
+        })
+    }else{
+        fetch(`${API_URL}?apiKey=${IP_API_KEY}&domain=${ipToFind}`).then( res => {
+            return res.json()
+        }).then( data => {
+            consumeAPI(data)
+        })
+    }
+}
+const consumeAPI = data => {
+    const {ip, location, isp} = data
+    const formattedLocation = `${location.city}, ${location.region} ${location.postalCode}`
+    dataValues[IP].innerText = ip
+    dataValues[LOCATION].innerText = formattedLocation
+    dataValues[TIMEZONE].innerText = `UTC ${location.timezone}`
+    dataValues[ISP].innerText = isp
+    refreshMap(location.lat,location.lng)
+}
+const refreshMap = (lat,lon) =>{
+    mapboxgl.accessToken = MAP_TOKEN
+    var map = new mapboxgl.Map({
+        container: 'mapid',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lon,lat], 
+        zoom: 13 
+    })    
+    let marker = new mapboxgl.Marker({
+        element: createMarker(),
+        anchor: 'bottom'
+    })
+    .setLngLat([lon, lat])
+    .addTo(map);
+}
+const createMarker = () => {
+    const marker = document.createElement('div')
+    marker.className = 'marker'
+    marker.style.backgroundImage = 'url(/images/icon-location.svg)'
+    marker.style.width = '46px';
+    marker.style.height ='55px';
+    return marker
 }
