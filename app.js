5dc6879f9d1d70ccb111b1be3e50f2ff1447386b
S F <sebastian.fariasb@usach.cl>
2020-09-26 21:22:24 -0300

    working map

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const ipRegExp = new RegExp(/^([0-9]{1,3}\.){3}[0-9]{1,3}$/,'i')
+const domainRegExp = new RegExp(/(?:(?:(?<thld>[\w\-]*)(?:\.))?(?<sld>[\w\-]*))\.(?<tld>[\w\-]*)/,'i')
+const MAP_TOKEN = 'pk.eyJ1IjoieGVvbnJpZGVyIiwiYSI6ImNrZmkydTBnNzAzd24yd3FiNXRzb21laGgifQ.owsPKYbZ0PT_6fgOqYQ_fg'
+const IP_API_KEY ='at_qCntz3vCXjuld1TlzkCPJwBwU2Z84'
+const API_URL = 'https://geo.ipify.org/api/v1'
 const search = document.querySelector('#search')
 const input = document.querySelector('.search-bar')
 const dataValues = document.querySelectorAll('h3')
@@ -5,46 +10,32 @@ const IP = 0
 const LOCATION = 1
 const TIMEZONE = 2
 const ISP = 3
-const ipRegExp = new RegExp('^([0-9]{1,3}\.){3}[0-9]{1,3}$','i')
-const domainRegExp = new RegExp('(?:(?:(?<thld>[\w\-]*)(?:\.))?(?<sld>[\w\-]*))\.(?<tld>[\w\-]*)','i')
-const API_URL = 'https://geo.ipify.org/api/v1'
-const MAP_TOKEN = 'pk.eyJ1IjoieGVvbnJpZGVyIiwiYSI6ImNrZmkydTBnNzAzd24yd3FiNXRzb21laGgifQ.owsPKYbZ0PT_6fgOqYQ_fg'
-const IP_API_KEY ='at_qCntz3vCXjuld1TlzkCPJwBwU2Z84'
 
-window.addEventListener("DOMContentLoaded", () => init());
+window.addEventListener('DOMContentLoaded', () => fetchLocation(''));
 search.addEventListener('submit', event => handleSubmit(event))
 
-const init = () => {
-    fetch(`${API_URL}?apiKey=${IP_API_KEY}`).then( res => {
-            return res.json()
-        }).then( data => {
-            consumeAPI(data)
-        })
-}
 const handleSubmit = e => {
     e.preventDefault()
-    const ipToFind= input.value
-    input.value = ''
-    if(ipRegExp.test(ipToFind) || domainRegExp.test(ipToFind)){
-        findIP(ipToFind)
-    }else{
-        alert('Please, enter a valid IP Address or domain')
+    if(input.value){
+        const ipToFind = input.value
+        input.value = ''
+        if(ipRegExp.test(ipToFind) || domainRegExp.test(ipToFind)){
+            fetchLocation(ipToFind)
+        }else{
+            alert('Please, enter a valid IP Address or domain')
+        }
     }
 }
-const findIP = ipToFind => {
-    if(ipRegExp.test(ipToFind)){
-        fetch(`${API_URL}?apiKey=${IP_API_KEY}&ipAddress=${ipToFind}`).then( res => {
-            return res.json()
-        }).then( data => {
-            consumeAPI(data)
-        })
-    }else{
-        fetch(`${API_URL}?apiKey=${IP_API_KEY}&domain=${ipToFind}`).then( res => {
-            return res.json()
-        }).then( data => {
-            consumeAPI(data)
-        })
-    }
+const fetchLocation = ipToFind => {
+    let paramName = ''
+    if(ipRegExp.test(ipToFind)) paramName ='&ipAddress='
+    if(domainRegExp.test(ipToFind)) paramName = '&domain='    
+    fetch(`${API_URL}?apiKey=${IP_API_KEY}${paramName}${ipToFind}`).then( res => {
+        if(res.ok) return res.json()
+        else throw Error(res.status)
+    }).then( data => {
+        consumeAPI(data)
+    }).catch( () => alert('Please, enter a valid IP Address or domain'))
 }
 const consumeAPI = data => {
     const {ip, location, isp} = data
@@ -53,11 +44,11 @@ const consumeAPI = data => {
     dataValues[LOCATION].innerText = formattedLocation
     dataValues[TIMEZONE].innerText = `UTC ${location.timezone}`
     dataValues[ISP].innerText = isp
-    refreshMap(location.lat,location.lng)
+    refreshMap(location.lng,location.lat)
 }
-const refreshMap = (lat,lon) =>{
+const refreshMap = (lon,lat) =>{
     mapboxgl.accessToken = MAP_TOKEN
-    var map = new mapboxgl.Map({
+    let map = new mapboxgl.Map({
         container: 'mapid',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [lon,lat], 
